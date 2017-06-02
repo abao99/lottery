f460aca2e324839f39f65b646d8fdebde40feb2c
abao99 <likepenguincz@gmail.com>
2017-06-02 18:03:21 +0800

    test2

--- test.php
+++ test.php
@@ -7,46 +7,158 @@ $buffer = file($url); //將網址讀入buffer變數
 		$d=strpos($buffer[$i],"每次竞猜选择一个选项下注"); //檢查要找的字,是否存在跟位置，判斷在哪一行
 		if($d !== false){
 
-			$date[]=substr($buffer[$i],$d-11,11); //11是日期的長度
+			//$date[]=substr($buffer[$i],$d-11,11); 11是日期的長度
+			preg_match_all("/<\/span>(.*)每次竞猜选择一个选项下注/",$buffer[$i],$date1,PREG_PATTERN_ORDER);
+			$date[]=$date1[1][0];
 		}
 
 		//抓賽事
 		$r=strpos($buffer[$i],"color: #ffffff"); //檢查要找的字,是否存在跟位置
 		if($r !== false){
-			$r1=strpos($buffer[$i],"</span>");//找結束位置
-			$race[]=substr($buffer[$i],$r+16,$r1-($r+16)); 
+			/*$r1=strpos($buffer[$i],"</span>");//找結束位置
+			$race[]=substr($buffer[$i],$r+16,$r1-($r+16)); */
+			preg_match_all("/color: #ffffff'>(.*)<\/span>/",$buffer[$i],$r1,PREG_PATTERN_ORDER);
+			$race[]=$r1[1][0];
 		}
 
-		//抓主隊
+		//抓主隊VS客隊
 		$h=strpos($buffer[$i],'class="ht"'); //檢查要找的字,是否存在跟位置
 		if($h !== false){
-			$h1=strpos($buffer[$i],"</span>vs");//找結束位置
-			$host[]=substr($buffer[$i],$h+11,$h1-($h+11)); 
-		}
-		
-		//抓客隊
-		$v=strpos($buffer[$i],'class="at"'); //檢查要找的字,是否存在跟位置
-		if($v !== false){
-			$v1=strpos($buffer[$i],"</span>");//找結束位置
-			$visite[]=substr($buffer[$i],$v+11,$v1-($v+7)); //
+			/*$h1=strpos($buffer[$i],"</span>vs");//找結束位置
+			$host[]=substr($buffer[$i],$h+11,$h1-($h+11));*/
+			preg_match_all('/<span class="ht">(.*)<\/span>/',$buffer[$i],$h1,PREG_PATTERN_ORDER);
+			$host[]=$h1[1][0]; 
 		}
 
 		//抓時間
 		$t=strpos($buffer[$i],'class="time"'); //檢查要找的字,是否存在跟位置
 		if($t !== false){
 			
-			$time[]=substr($buffer[$i],$t+13,5); //
+			//$time[]=substr($buffer[$i],$t+13,5); 
+			preg_match_all('/class="time">(.*)<\/td>/',$buffer[$i],$t1,PREG_PATTERN_ORDER);
+			$time[]=$t1[1][0];
 		}
 
 		//抓讓球
 		$c=strpos($buffer[$i],'class="rq2"'); //檢查要找的字,是否存在跟位置
 		if($c !== false){
+			/*
 			$c1=strpos($buffer[$i],'">');//找開始位置
-			
 			$concede[]=substr($buffer[$i],$c1+2,2); //
+			*/
+			preg_match_all('/[0-9]/',$buffer[$i],$c1,PREG_PATTERN_ORDER);
+			$concede[]=$c1[1][0];
+		}
+
+		//主勝
+		$victory1=strpos($buffer[$i],'<span>胜'); //檢查要找的字,是否存在跟位置
+		if($victory1 !== false){
+			
+			$victory[]=substr($buffer[$i],$victory1+9,5); //
+		}
+
+		//平局
+		$draw1=strpos($buffer[$i],'<span>平'); //檢查要找的字,是否存在跟位置
+		if($draw1 !== false){
+			
+			$draw[]=substr($buffer[$i],$draw1+9,5); //
+		}
+
+		//負
+		$defeat1=strpos($buffer[$i],'<span>负'); //檢查要找的字,是否存在跟位置
+		if($defeat1 !== false){
+			
+			$defeat[]=substr($buffer[$i],$defeat1+9,5); //
+		}
+
+		//競猜人數
+		$num1=strpos($buffer[$i],'class="num"'); //檢查要找的字,是否存在跟位置
+		if($num1 !== false){
+			
+			$num2=strpos($buffer[$i],'</td>');
+			$num[]=substr($buffer[$i],$num1+12,$num2-($num1+10)); //
 		}
 		
 	}
-	echo $concede[1]."<br>";
+	echo $date[0]."<br>";
 	
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>lottery</title>
+</head>
+<body>
+	<table>
+  		<tr>
+		    <th>赛事</th>
+		    <th> 主队   vs   客队</th>
+		    <th>截止</th>
+		    <th>让球</th>
+		    <th>主胜</th>
+		    <th>平局</th>
+		    <th>客胜</th>
+		    <th>竞猜人数</th>
+  		</tr>
+  		
+	</table>
+	<table>
+		<tr>
+    		<td colspan="8">
+    			<?php echo $date[0]; ?>		
+    		</td>
+  		</tr>
+
+		<tr>
+			<td rowspan="2">
+				<?php echo $race[0]; ?>
+			</td>
+			
+			<td rowspan="2">
+				<?php echo $host[0]  ?>
+			</td>
+			
+			<td rowspan="2">
+				<?php echo $time[0]; ?>
+			</td>
+					
+			<td>
+				0
+			</td>
+				
+			<td>
+				<?php echo "胜".$victory[0]; ?>
+			</td>
+			
+			<td>
+				<?php echo "平".$draw[0]; ?>
+			</td>
+			
+			<td>
+				<?php echo "负".$defeat[0]; ?>
+			</td>
+			
+			<td rowspan="2">
+				<?php echo $num[0]; ?>
+			</td>
+		</tr>
+
+		<tr>
+			<td>
+				<?php echo $concede[0]; ?>
+			</td>
+			<td>
+				<?php echo "胜".$visite[0]; ?>
+			</td>
+			<td>
+				<?php echo "平".$draw[0]; ?>
+			</td>
+			<td>
+				<?php echo "负".$defeat[0]; ?>
+			</td>
+		</tr>
+	</table>
+
+</body>
+</html>
---
+++ test2.php
@@ -0,0 +1,53 @@
+<?php
+
+$url = "http://www.kufa88.com/Promotion/jingcai";
+$arr= file_get_contents($url); //抓網站內容到變數
+
+//取消換行
+$str = $arr;
+$order   = array("\r\n", "\n", "\r");
+$replace = "";
+$newarr = str_replace($order, $replace, $str);
+
+//抓tbody內容
+preg_match_all('/<tbody>(.*)<\/tbody>/',$newarr,$tbody,PREG_PATTERN_ORDER);
+//echo $tbody[1][0];
+
+//抓日期資料
+preg_match_all('/<td colspan="6"><span class="moreIcon"><\/span>(.*?)每次竞猜选择一个选项下注/',$tbody[0][0],$date,PREG_PATTERN_ORDER);
+echo $date[1][0];	
+
+//抓第一天賽事
+preg_match_all('/<tr class="moreContent more1" style="display: table-row">(.*?)<\/tr>/',$tbody[0][0],$day1,PREG_PATTERN_ORDER);
+echo $day1[1][1]."<br>";
+
+$count =count($day1[1]);
+for($i=0;$i<$count;$i++){
+
+	//抓賽事
+	preg_match_all("/color: #ffffff'>(.*?)<\/span><\/td>/",$day1[1][$i],$race,PREG_PATTERN_ORDER);
+	//echo $race[1][0]."<br>";
+
+	//抓主隊
+	preg_match_all('/<span class="ht">(.*)<\/span>vs/',$day1[1][$i],$host,PREG_PATTERN_ORDER);
+	//echo $host[1][0]."<br>";
+
+	//抓客隊
+	preg_match_all('/<span class="at">(.*?)<\/span>/',$day1[1][$i],$visite,PREG_PATTERN_ORDER);
+	//echo $visite[1][0]."<br>";
+
+	//抓時間
+	preg_match_all('/<td class="time">(.*?)<\/td>/',$day1[1][$i],$time,PREG_PATTERN_ORDER);
+	//echo $time[1][0]."<br>";
+
+	//抓讓球
+	preg_match_all('/<span class="rq2"(.*?)<\/span>/',$day1[1][$i],$concede,PREG_PATTERN_ORDER);
+	//echo $concede[0][0]."<br>";
+
+	//抓勝
+	preg_match_all('/<span>胜(.*?)<input/',$day1[1][$i],$victory,PREG_PATTERN_ORDER);
+	echo $victory[1][0]."<br>";
+}
+
+
+?>
