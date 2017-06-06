660a1764dcf8f9f2fe30993038899d50bdcce4d3
abao99 <likepenguincz@gmail.com>
2017-06-06 18:22:22 +0800

    存資料庫

--- test2.php
+++ test2.php
@@ -1,8 +1,7 @@
 <?php
-
-//$url = "http://www.kufa88.com/Promotion/jingcai"; //足球
+$url = "http://www.kufa88.com/Promotion/jingcai"; //足球
 //$url = "http://localhost/source3.html"; //NBA
-$url = "http://localhost/source4.html"; //足球
+//$url = "http://localhost/source4.html"; //足球
 $arr= file_get_contents($url); //抓網站內容到變數
 
 //取消換行
@@ -32,15 +31,17 @@ for($i=1; $i<=count($date[1]); $i++) {
 
 function game($game,$d){
 		$count1 =count($game[1]);
+    $count2 = $count1-1;
 		$d=$d-1;					//$d 第幾天
 		$data = array();
+    global $date;
 
 
 
 	for($i=0;$i<$count1;$i++){	//$i 第幾場賽事
 
 		//抓賽事
-		preg_match_all('/color: #ffffff">(.*?)<\/span><\/td>/',$game[1][$i],$race,PREG_PATTERN_ORDER);
+		preg_match_all('/color: #FFFFFF\'>(.*?)<\/span><\/td>/',$game[1][$i],$race,PREG_PATTERN_ORDER);
 		//echo $race[1][0].",";
 		$data[$d][$i]["race"]=$race[1][0];
 		//echo $data[$d][$i]["race"].",";
@@ -70,7 +71,7 @@ function game($game,$d){
 		//echo $concede[1][0].",";
 		$data[$d][$i]["concede"]=$concede[1][0];		
 		//echo $data[$d][$i]["concede"].",";
-		
+
 		//抓勝
 		preg_match_all('/<span>胜(.*?)<input/',$game[1][$i],$victory,PREG_PATTERN_ORDER);
 		
@@ -124,12 +125,68 @@ function game($game,$d){
 		//echo $num[1][0]."<br>";
 			
 	}
-	
-	
+			
+  $link = mysqli_connect('localhost','root','123456','lottery') // 連資料庫
+			or die("error".mysqli_connect_error());
+  mysqli_query($link,"set names utf8");
+
+ 
+                                     // 查詢判斷重複
+  $sql_insert = "INSERT INTO game (date, race, host, visite, time, concede, victory, victory1, draw, draw1, defeat, defeat1, num) VALUES ";
+	 
 	foreach($data as $d => $v) {
     	
     	foreach($v as $i => $v2) {
-    		echo $data[$d][$i]["race"].",";
+               
+        $sql = "select * from game where date = ".$date[1][$d]." and      
+                                   race =".$data[$d][$i]["race"]." and
+                                   host =".$data[$d][$i]["host"]." and
+                                   visite =".$data[$d][$i]["visite"]." and
+                                   time =".$data[$d][$i]["time"];
+        
+        $result = mysqli_query($link,$sql);//查詢判斷重複 
+
+        //把資料存到rowarry陣列
+        $row[] = mysqli_fetch_assoc($result); 
+        
+         if($result){
+            
+            echo "a";
+           // $sql_update = "UPDATE game SET num=".$data[$d][$i]['num']." WHERE id=."$rowarray[$j]['id'];
+           // mysqli_query($link, $sql_update);
+            
+          }// end if
+          else{
+            $sql_insert .= "('".$date[1][$d]."',
+                               '".$data[$d][$i]["race"]."',
+                               '".$data[$d][$i]["host"]."',
+                               '".$data[$d][$i]["visite"]."',
+                               '".$data[$d][$i]["time"]."',
+                               '".$data[$d][$i]["concede"]."',
+                               '".$data[$d][$i]["victory"]."',
+                               '".$data[$d][$i]["victory1"]."',
+                               '".$data[$d][$i]["draw"]."',
+                               '".$data[$d][$i]["draw1"]."',
+                               '".$data[$d][$i]["defeat"]."',
+                               '".$data[$d][$i]["defeat1"]."',
+                               '".$data[$d][$i]["num"]."'
+                              ),";
+          }//end else
+       //substr($query, 0, -1);
+
+    	}//end foreach $v
+  	}//end foreach $data 
+    $sql_insert = substr($sql_insert, 0, -1);    
+    echo $sql_insert."<br>";
+    //echo $sql_update."<br>";
+    mysqli_query($link, $sql_insert);
+    
+   mysqli_close($link);
+}//end fouction
+
+
+/*
+echo $data[$d][$i]["race"].",";
     		echo $data[$d][$i]["host"].",";
     		echo $data[$d][$i]["visite"].",";
     		echo $data[$d][$i]["time"].",";
@@ -141,10 +198,18 @@ function game($game,$d){
     		echo "负".$data[$d][$i]["defeat"].",";
     		echo "负".$data[$d][$i]["defeat1"].",";
     		echo $data[$d][$i]["num"]."<br>";
-    	}
-  	}
-  	
-
-}
-
+        
+        
+        $rowarray[$j]["time"] != $data[$d][$i]["time"] &&
+             $rowarray[$j]["concede"] != $data[$d][$i]["concede"] &&
+             $rowarray[$j]["victory"] != $data[$d][$i]["victory"] &&
+             $rowarray[$j]["victory1"] != $data[$d][$i]["victory1"] &&
+             $rowarray[$j]["draw"] != $data[$d][$i]["draw"] &&
+             $rowarray[$j]["draw1"] != $data[$d][$i]["draw1"] &&
+             $rowarray[$j]["defeat"] != $data[$d][$i]["defeat"] &&
+             $rowarray[$j]["defeat1"] != $data[$d][$i]["defeat1"] &&
+             $rowarray[$j]["num"] != $data[$d][$i]["num"] 
+             */
+             
+   
 ?>
